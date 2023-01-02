d6dfdbf754959e6eb004881a3849bc7b0bec0da5
infamous55 <infamouslegend555@gmail.com>
2023-01-02 18:50:53 +0200

    added custom 404

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,8 +11,8 @@ const Layout: React.FC<Props> = ({ children }) => {
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="min-h-screen w-full bg-neutral-900 text-white">
-        <div className="m-auto w-11/12 md:w-8/12">
+      <div className="w-full bg-neutral-900 text-white">
+        <div className="m-auto flex min-h-screen w-11/12 flex-col md:w-8/12">
           <NavBar />
           {children}
         </div>
---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout";
+
+const NotFound: React.FC = () => {
+  return (
+    <Layout>
+      <div className="flex h-full w-full flex-grow items-center justify-center">
+        <span className="inline-block">
+          <h3 className="mb-2 text-center text-5xl font-semibold">404</h3>
+          <p>This page could not be found.</p>
+        </span>
+      </div>
+    </Layout>
+  );
+};
+
+export default NotFound;
