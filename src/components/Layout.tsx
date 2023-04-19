d6caa8604cdf944f04b67b885b1c26e12501714a
infamous55 <infamouslegend555@gmail.com>
2023-04-19 17:59:21 +0300

    modified layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,21 +7,16 @@ type Props = {
 
 const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="min-h-screen w-full bg-neutral-900 text-white">
-        <div className="mb-4 flex w-full items-center justify-center border-b border-gray-600">
-          <div className="z-10 w-11/12 md:w-8/12">
-            <NavBar />
-          </div>
-        </div>
-        <div className="m-auto flex w-11/12 flex-col overflow-x-hidden pb-12 md:w-8/12">
-          {children}
+    <div className="min-h-screen w-full bg-neutral-900 text-white">
+      <div className="mb-4 flex w-full items-center justify-center border-b border-gray-600">
+        <div className="z-10 w-11/12 md:w-8/12">
+          <NavBar />
         </div>
       </div>
-    </>
+      <div className="m-auto flex w-11/12 flex-col overflow-x-hidden pb-12 md:w-8/12">
+        {children}
+      </div>
+    </div>
   );
 };
 
